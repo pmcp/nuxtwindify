a8e8f31f341a7a1ed6f5e8ef9bc98503b702611c
PMCP <maartenlauwaert@gmail.com>
2021-02-12 16:44:18 +0000

    Added netlify functions

---
+++ functions/exampleFunction/exampleFunction.js
@@ -0,0 +1,8 @@
+var _ = require('lodash')
+// const items = require('../shared/items.json')
+
+exports.handler = async function(event, context) {
+  // These come from the call
+  const parameters = event.queryStringParameters
+  return
+}
